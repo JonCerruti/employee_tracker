2c60fbd606c06cad15eb56ca55ad370a28c66897
Jon <jonathancerruti@gmail.com>
2022-08-15 22:37:07 -0700

    db created

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS team_db;
+CREATE DATABASE team_db;
+USE team_db;
+
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    manager_id INT,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+)
