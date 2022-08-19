013fa9bf821591b1b8761cd2441430acb9de7c45
Jon <jonathancerruti@gmail.com>
2022-08-18 17:34:21 -0700

    display roles, employees, departments, and adding employees working

--- db/schema.sql
+++ db/schema.sql
@@ -7,12 +7,12 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department (id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -20,7 +20,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     manager_id INT,
-    role_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
+    roles_id INT,
+    FOREIGN KEY (roles_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 )
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const consoleTable = require('console.table');
-const { listenerCount } = require('mysql2/typings/mysql/lib/Connection');
+
 
 //connection to sql db
 const connection = mysql.createConnection({
@@ -19,7 +19,7 @@ connection.connect(function(err){
 promptConsole = () =>{
     console.log('************************')
     console.log('*                      *')
-    console.log('*   Employee Manger    *')
+    console.log('*   Employee Manager   *')
     console.log('*                      *')
     console.log('************************')
     promptEmployeeTracker();
@@ -46,38 +46,142 @@ function promptEmployeeTracker(){
         ]
     })
     .then((answers) => {
-        const { choices } = answers;
-        if (choices === 'View all employees'){
+        const { options } = answers;
+        if (options === 'View all employees'){
             displayEmployees();
         }
-        if (choices === ' Add and employee'){
+        if (options === 'Add an employee'){
             addEmployee();
         }
-        if (choices === 'View all roles'){
+        if (options === 'View all roles'){
             displayRoles();
         }
-        if (choices === 'Add a role'){
+        if (options === 'Add a role'){
             addRole();
         }
-        if (choices === 'View all departments'){
+        if (options === 'View all departments'){
             displayDepartments();
         }
-        if (choices === 'Add a department'){
+        if (options === 'Add a department'){
             addDepartment();
         }
-        if (choices === 'Update an employee role'){
+        if (options === 'Update an employee role'){
             updateEmploeeRole();
         }
-        if (choices === 'Quit'){
+        if (options === 'Quit'){
             connection.end()
         };
     });
 };
 
-displayEmployees = () => {
+displayEmployees = () =>  {
+    console.log('Displaying all employees...\n');   
+    const sql = `SELECT employee.id, 
+                    employee.first_name, 
+                    employee.last_name, 
+                    roles.title, 
+                    department.name AS department,
+                    roles.salary,
+                    CONCAT (manager.first_name, " ", manager.last_name) AS manager
+                    FROM employee
+                    LEFT JOIN roles ON employee.roles_id = roles.id
+                    LEFT JOIN department ON roles.department_id = department.id
+                    LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+    connection.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptEmployeeTracker();
+        
+    })
+};
+
+const displayRoles = () => {
+    const sql = `SELECT roles.id AS "Roles ID", roles.title AS Roles, roles.salary AS Salary, roles.department_id AS "Department ID" FROM employee_tracker_db.roles`;
+    connection.query(
+      sql,
+      (err, rows) => {
+        if (err) throw err;
+        console.log('\n');
+        console.table(rows);
+        promptEmployeeTracker();
+    })
+  }
+
+displayDepartments = () => {
+    const sql = `SELECT department.id AS "Department ID", department.name AS department FROM employee_tracker_db.department`;
+
+    connection.query(
+        sql,
+        (err, rows) => {
+        if (err) throw err;
+        console.log('\n')
+        console.table(rows);
+        promptEmployeeTracker();
+    })
+};
+
+
+
+const addEmployee = async () => {
+    connection.query('Select * FROM roles', async (err, roles) => {
+        if (err) throw err; 
+        
+        connection.query('Select * FROM employee WHERE manager_id IS NULL', async (err, managers) => {
+          if (err) throw err; 
     
+        managers = managers.map(manager => ({name:manager.first_name + " " + manager.last_name, value: manager.id}));
+        managers.push({name:"None"});
+    
+        const responses = await inquirer
+    
+    .prompt([
+        {
+            type: 'input',
+            name: 'firstname',
+            message: 'What is the employees first name?',
+            
+        },
+        {
+            type : 'input',
+            name: 'lastname',
+            message: 'What is your employees last name?',
+           
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is the employees role?',
+            choices: roles.map(roles => ({name:roles.title, value: roles.id})),
+        },
+        {
+            type: 'rawlist',
+            name: 'managers',
+            message: 'What is their managers name?',
+            choices: managers
+        }   
+    ])
+    if (responses.managers === "None") {
+        responses.managers = null;
+      }
+
+    connection.query(
+        'INSERT INTO employee SET ?',
+        {
+          first_name: responses.firstname,
+          last_name: responses.lastname,
+          roles_id: responses.role,
+          manager_id: responses.managers
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log("New employee added.\n");
+          promptEmployeeTracker();
+        }
+    )
+  })
+})
 }
 // function to view departments 
 // function to view roles 
 // function to view employees
-//
+
